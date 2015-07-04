19ca584dab9f74ddfa43beb0efe4ef3c953be7fa
ldjking <ldjbox@126.com>
2015-07-04 11:39:21 +0800

    提交代码

---
+++ hello/www/user/js/libs/zepto/zepto.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>页面不存在</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta name="format-detection" content="telephone=no,address=no,email=no">
+<meta name="mobileOptimized" content="width">
+<meta name="handheldFriendly" content="true">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="/css/style.css?v=20150524" />
+<style>
+	.npx_activity{
+		position:relative;
+	}
+	.npx_activity img{
+		position:absolute;
+		left:50%;
+		margin-left:-45px;
+		width:82px;
+		top:-6px;
+		height:auto!important;
+		background-color:#F00;
+	}
+</style>
+<title>农发贷</title>
+<script>
+var _hmt = _hmt || [];
+(function() {
+	var host=document.location.hostname;
+	if(/nongfadai.com$/ig.test(host)){
+	  var hm = document.createElement("script");
+	  hm.src = "//hm.baidu.com/hm.js?10cd9976402aabfbd36d69dd43dd8c1a";
+	  var s = document.getElementsByTagName("script")[0]; 
+	  s.parentNode.insertBefore(hm, s);
+	}
+})();
+</script>
+<script src="/js/wording.js"></script></head>
+<body style="background:#e7e6eb;">
+<div style="width:599px; height:306px; background:url('/user/images/404.jpg') no-repeat; margin:150px auto 0 auto; text-align:center; font-size:14px; font-family:Microsoft yahei; padding-top:180px; line-height:28px; color:#666;">
+所访问的页面不存在！<br />
+您可以返回<a href="http://www.nongfadai.com" style="color:#00a7e5;">农发贷</a>首页查找您感兴趣的内容，或者请联系客服 4008-2255-38<br />
+管理员为网站给您带来不便致以诚挚的歉意！
+</div>
+</body>
+</html>
